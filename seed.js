fc3b4a30fbccc68e1eb02f64e8aac0566b390ffc
Zeke Sikelianos <zeke@sikelianos.com>
2018-01-15 08:52:21 -0800

    fresh db seeded with package names

---
+++ seed.js
@@ -0,0 +1,12 @@
+const db = require('level')('./db')
+const names = require('all-the-package-names')
+
+for (i = 0; i < names.length; i++) {
+  const name = names[i]
+  console.log(i, name)
+  saveName(name)
+}
+
+async function saveName (name) {
+  await db.put(name, {})
+}
